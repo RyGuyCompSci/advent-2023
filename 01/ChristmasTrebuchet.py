9c070ce1dfa55e849a27a75529227c19e09d29a1
Ryan Jones <ryanjones@dornerworks.com>
2023-12-04 08:18:37 -0500

    Solve first puzzle

---
+++ 01/ChristmasTrebuchet.py
@@ -0,0 +1,21 @@
+import re
+
+file_name = "input.txt"
+
+def get_file(name):
+  return open(name, "r")
+
+def solution():
+  file = get_file(file_name)
+  sum = 0
+
+  for line in file:
+    line = re.sub("([A-z])|(\n)", "", line)
+    first = line[0]
+    last = line[len(line) - 1]
+    sum = sum + int(first + last)
+  file.close()
+  print(sum)
+
+
+solution()
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# advent-2023
+# Advent of Code 2023
 Code Repository for my solutions to the Advent of Code 2023
